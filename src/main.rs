526f278d12e418654d40c8d486a119416863bf63
narumincho <16481886+narumincho@users.noreply.github.com>
2025-09-06 17:35:25 +0900

    アルファ値を使わない

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::boxed::box_new;
-
-use image::{self, Pixel};
+use image;
 
 // 画像を読み込む
 fn main() {
@@ -11,18 +9,17 @@ fn main() {
     let mut pixels: Vec<bool> = vec![];
 
     for pixel in img.pixels() {
-        pixels
-            .push(pixel[3] > 0 && (pixel[0] as u16 + pixel[1] as u16 + pixel[2] as u16) / 3 < 230);
+        pixels.push((pixel[0] as u16 + pixel[1] as u16 + pixel[2] as u16) / 3 < 230);
     }
 
-    let mut output_image = image::ImageBuffer::new(img.width(), img.height());
+    let mut output_image =
+        image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::new(img.width(), img.height());
 
     for (index, pixel) in output_image.pixels_mut().enumerate() {
         let value = pixels[index];
         pixel[0] = if value { 0 } else { 255 };
         pixel[1] = if value { 0 } else { 255 };
         pixel[2] = if value { 0 } else { 255 };
-        pixel[3] = 255;
     }
 
     // 画像を保存
