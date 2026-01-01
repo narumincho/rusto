d08feaf101a4ab306f4a400b8380d5973dd3439e
narumincho <16481886+narumincho@users.noreply.github.com>
2026-01-01 20:12:15 +0900

    Notionからページを取得

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
+mod notion_minecraft_db;
+
 use std::net::SocketAddr;
-
-use axum::{body, http::request};
-
-async fn get_notion_api_key() -> String {
-    match std::env::var("NOTION_KEY") {
-        // in Cloud Run
-        Ok(val) => val,
-        // local dev
-        Err(_) => std::fs::read_to_string("./notionApiKey.txt").unwrap(),
-    }
-}
 
 #[tokio::main]
 pub async fn main() {
@@ -22,7 +13,10 @@ pub async fn main() {
 
     println!("Listening on http://{}", addr);
 
-    let app = axum::Router::new().route("/", axum::routing::get(get_handler).post(post_handler));
+    let app = axum::Router::new().route(
+        "/",
+        axum::routing::get(get_handler).post(notion_minecraft_db::post_handler),
+    );
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -30,15 +24,4 @@ pub async fn main() {
 
 async fn get_handler() -> String {
     "Hello, World!".to_string()
-}
-
-async fn post_handler(request: axum::extract::Request) -> String {
-    let body_bytes = axum::body::to_bytes(request.into_body(), usize::MAX)
-        .await
-        .unwrap();
-    // Bytes を String に変換（UTF-8 として扱う）
-    let body_as_string = String::from_utf8(body_bytes.to_vec()).unwrap();
-    // 例: String を表示または返す
-    println!("Body as String: {}", body_as_string);
-    body_as_string
 }
--- src/notion_minecraft_db.rs
+++ src/notion_minecraft_db.rs
@@ -1,1 +1,82 @@
+use std::{collections::HashMap, iter::Map};
 
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionRequestBody {
+    data: NotionPageObject,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionPageObject {
+    parent: NotionPageParent,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionPageParent {
+    data_source_id: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionDatabaseResponse {
+    results: Vec<NotionPageObjectWithProperties>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionPageObjectWithProperties {
+    properties: NotionPropertyMap,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionPropertyMap {
+    ユーザー名: NotionProperty,
+    ユーザーID: NotionProperty,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct NotionProperty {
+    id: String,
+}
+
+pub async fn post_handler(
+    axum::extract::Json(payload): axum::extract::Json<NotionRequestBody>,
+) -> &'static str {
+    let data_source_id = &payload.data.parent.data_source_id;
+    println!("Received payload: {:?}", data_source_id);
+    let url = reqwest::Url::parse_with_params(
+        &format!(
+            "https://api.notion.com/v1/data_sources/{}/query",
+            data_source_id
+        ),
+        &[
+            ("filter_properties[]", "ユーザー名"),
+            ("filter_properties[]", "ユーザーID"),
+        ],
+    )
+    .unwrap();
+    let result = reqwest::Client::new()
+        .post(url)
+        .header("Notion-Version", "2025-09-03")
+        .header(
+            "Authorization",
+            format!("Bearer {}", get_notion_api_key().await),
+        )
+        // TODO pagination
+        .send()
+        .await
+        .unwrap()
+        .json::<NotionDatabaseResponse>()
+        .await
+        .unwrap();
+    for page in &result.results {
+        println!("Page properties: {:?}", page.properties);
+    }
+    "ok"
+}
+
+async fn get_notion_api_key() -> String {
+    match std::env::var("NOTION_KEY") {
+        // in Cloud Run
+        Ok(val) => val,
+        // local dev
+        Err(_) => std::fs::read_to_string("./notionApiKey.txt").unwrap(),
+    }
+}
